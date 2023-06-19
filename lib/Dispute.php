f767ed726c504336fd8b1e7365871a8637f31508
Octocat <135540427+octocat0415@users.noreply.github.com>
2023-06-19 15:42:46 -0400

    Update Dispute.php

--- lib/Dispute.php
+++ lib/Dispute.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * @author Victor Kislichenko <v.kislichenko@gmail.com>
- * Created at 01/06/2020 16:45 AM UTC+03:00
- *
- * @see https://shopify.dev/docs/admin-api/rest/reference/shopify_payments/dispute Shopify API Reference for Dispute
- */
 
 namespace PHPShopify;
 
