2e3e99160ae1c17332a1e7ab47a3e64f59efecd9
octocat0415 <foryoursuccess0415@gmail.com>
2023-06-27 22:26:49 -0400

    Update

--- lib/Fulfillment.php
+++ lib/Fulfillment.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * @author Tareq Mahmood <tareqtms@yahoo.com>
- * Created at 8/19/16 3:04 PM UTC+06:00
- *
- * @see https://help.shopify.com/api/reference/fulfillment Shopify API Reference for Fulfillment
- */
 
 namespace PHPShopify;
 
