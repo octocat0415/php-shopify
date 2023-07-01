bbc4fa22dcd71c4ee4bedd54ac6f16b6dd5085c3
Octocat <135540427+octocat0415@users.noreply.github.com>
2023-07-01 08:29:13 -0400

    Update Shop.php

--- lib/Shop.php
+++ lib/Shop.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * @author Tareq Mahmood <tareqtms@yahoo.com>
- * Created at 8/19/16 10:42 AM UTC+06:00
- *
- * @see https://help.shopify.com/api/reference/shop Shopify API Reference for Shop
- */
 
 namespace PHPShopify;
 
